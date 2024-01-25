9d52638426214733417dd3d8ab95ff8b7b7c7c6f
OzzieTheGreat <90358793+OzzieTheGreat@users.noreply.github.com>
2024-01-25 01:51:51 -0500

    Update Snake.ts

--- sandbox/src/Snake.ts
+++ sandbox/src/Snake.ts
@@ -1,23 +1,59 @@
-
+import Point from "./Point";
+import WorldModel from "./WorldModel";
 class Snake {
   private currentPosition: number;
   private currentDirection: string;
-  constructor(snakeDirection: string) {
+  private newPoint: Point;
+  private wor: WorldModel;
+  constructor(snakeDirection: string, w: WorldModel) {
     this.currentPosition = 0;
     this.currentDirection = snakeDirection;
+    this.newPoint = new Point(0, 0);
+    this.wor = w;
   }
   public move(position: number) {
-    if (this.currentDirection === "right") {
+    if (this.currentDirection === "left") {
       this.currentPosition = position + 1;
-    } else if (this.currentDirection === "left") {
+    } else if (this.currentDirection === "right") {
+      this.currentPosition = position - 1;
+    }
+    if (this.currentDirection === "up") {
+      this.currentPosition = position + 1;
+    }
+    if (this.currentDirection === "down") {
       this.currentPosition = position - 1;
     }
   }
+  /**
+   * @deprecated Use {turn}
+   */
   public turn(direction: string) {
-    if (direction === "foward") {
-      this.currentDirection = "foward";
-    } else if (direction === "backward") {
-      this.currentDirection = "backward";
+    if (direction === "up") {
+      this.currentDirection = "up";
+    } else if (direction === "down") {
+      this.currentDirection = "dowm";
+    }
+  }
+  turnLeft() {
+    if (this.currentDirection === "left") {
+      this.currentDirection = "dowm";
+    } else if (this.currentDirection === "right") {
+      this.currentDirection = "up";
+    } else if (this.currentDirection === "up") {
+      this.currentDirection = "left";
+    } else if (this.currentDirection === "down") {
+      this.currentDirection = "right";
+    }
+  }
+  turnRight() {
+    if (this.currentDirection === "left") {
+      this.currentDirection = "up";
+    } else if (this.currentDirection === "right") {
+      this.currentDirection = "dowm";
+    } else if (this.currentDirection === "up") {
+      this.currentDirection = "right";
+    } else if (this.currentDirection === "down") {
+      this.currentDirection = "left";
     }
   }
   public get position() {
@@ -25,6 +61,9 @@ class Snake {
   }
   public get direction() {
     return this.currentDirection;
+  }
+  public set worldmodel(w: WorldModel) {
+    this.wor = w;
   }
 }
 
