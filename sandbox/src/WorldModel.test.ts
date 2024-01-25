1571dee0240229f91ac5ec27da66911b4be43928
OzzieTheGreat <90358793+OzzieTheGreat@users.noreply.github.com>
2024-01-25 01:53:15 -0500

    Create WorldModel.test.ts

---
+++ sandbox/src/WorldModel.test.ts
@@ -0,0 +1,10 @@
+//import WorldModel from "./WorldModel";
+/*
+import Snake from "./Snake";
+const Modelworld = (times: number) => {
+  const worldModel = new WorldModel();
+  let greenSnake = new snake("green", worldModel);
+  let maroonSnake = new snake("maroon", worldModel);
+  let blueSnake = new snake("blue", worldModel);
+  */
+export {};
