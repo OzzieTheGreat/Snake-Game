dea40bc1b7f1d35c6f039b17340e945f66322a38
OzzieTheGreat <90358793+OzzieTheGreat@users.noreply.github.com>
2024-01-29 14:50:10 -0500

    Update App.tsx

--- sandbox/src/App.tsx
+++ sandbox/src/App.tsx
@@ -2,26 +2,38 @@ import "./App.css";
 import "./App.css";
 import Snake from "./Snake";
 import display from "./display";
+import WorldModel from "./WorldModel";
 import { useEffect } from "react";
 
 export default function App() {
   // Add Snake Tests with display below
   useEffect(() => {
-    const greenSnake = new Snake("left");
-    const maroonSnake = new Snake("right");
-    const blueSnake = new Snake("down");
+    const greenSnakeWorld = new WorldModel("green", "left");
+    const maroonSnakeWorld = new WorldModel("maroon", "right");
+    const blueSnakeWorld = new WorldModel("blue", "down");
+    const greenSnake = new Snake("left", greenSnakeWorld);
+    const maroonSnake = new Snake("right", maroonSnakeWorld);
+    const blueSnake = new Snake("down", blueSnakeWorld);
+    greenSnakeWorld.update(1);
+    maroonSnakeWorld.update(2);
+    blueSnakeWorld.update(3);
     greenSnake.turn("left");
     greenSnake.move(5);
     maroonSnake.turn("right");
     maroonSnake.move(3);
     blueSnake.turn("right");
     blueSnake.move(7);
+    console.log(greenSnake);
     console.log(greenSnake.position);
     console.log(maroonSnake.position);
     console.log(blueSnake.position);
     console.log(greenSnake.direction);
     console.log(maroonSnake.direction);
     console.log(blueSnake.direction);
+    console.log(greenSnakeWorld.Snakes);
+    console.log(maroonSnakeWorld.Snakes);
+    console.log(blueSnakeWorld.Snakes);
+    console.log(greenSnakeWorld.update);
     // Include your display statements to test below
     document.getElementById("output")!.innerText = "OUTPUT:\n";
     display("hey");
