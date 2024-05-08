5a9d5e08fe55320ca2ce48cc0bfdd923ea6fab2a
OzzieTheGreat <90358793+OzzieTheGreat@users.noreply.github.com>
2024-05-08 14:36:18 -0400

    Create ArrayIterator.ts

---
+++ sandbox/src/ArrayIterator.ts
@@ -0,0 +1,21 @@
+import display from './display';
+import Snake from './Snake';
+import Food from './Food';
+
+class ArrayIterator<T>{
+  private arr:T[];
+  private index:number;
+  constructor(array:T[]) {
+    this.arr = array;
+    this.index = 0;
+  }
+  public next() {
+    let obj = {value: this.arr[this.index], done: this.index === this.arr.length};
+    if(this.index !== this.arr.length) {
+    this.index = this.index + 1;
+    }
+    return obj;
+  }
+}
+
+export default ArrayIterator;
