4be9343013fd6eeec5dcfa2f90436d0028dd5126
OzzieTheGreat <90358793+OzzieTheGreat@users.noreply.github.com>
2024-05-08 14:39:36 -0400

    Update GameController.ts

--- sandbox/src/GameController.ts
+++ sandbox/src/GameController.ts
@@ -1,13 +1,24 @@
 import WorldModel from "./WorldModel";
 import Player from "./Player";
+import Snake from "./Snake";
+import SnakeController from "./SnakeController";
+import SnakeFoodCollisionHandler from "./SnakeFoodCollisionHandler";
+import LRKeyInputHandler from "./LRKeyInputHandler";
+import HumanPlayer from "./HumanPlayer";
+import AvoidWallsPlayer from "./AvoidWallsPlayer";
+import CanvasWorldView from "./CanvasWorldView";
+import WorldLoader from "./WorldLoader";
+import Game from "./Game";
 class GameController {
   private world: WorldModel;
   private player1: Player;
   private player2: Player;
-  constructor(world: WorldModel, player1: Player, player2: Player) {
+  private game: Game;
+  constructor(world: WorldModel, player1: Player, player2: Player, g: Game) {
     this.world = world;
     this.player1 = player1;
     this.player2 = player2;
+    this.game = g;
   }
   public set Player1(player: Player) {
     this.player1 = player;
@@ -15,7 +26,6 @@ class GameController {
   public set Player2(player: Player) {
     this.player2 = player;
   }
-
   public run() {
     let lastTime = 0;
     let updateFrame = (mili: number) => {
@@ -24,10 +34,42 @@ class GameController {
       if (mili > lastTime + 250) {
         this.world.update(1);
         lastTime = lastTime + 250;
+      } else {
+        this.game.players = [];
+        this.game.world.dispose();
+        this.game.switchContext({});
       }
       requestAnimationFrame(updateFrame);
     };
     requestAnimationFrame(updateFrame);
+  }
+
+  public init(data: Game) {
+    const totalPlayers = data.numOfHumanPlayers + data.numOfHumanPlayers;
+    const players: Player[] = [];
+
+    const worldLoader = new WorldLoader();
+    const foodData = ["f".repeat(data.foodCount)]; // Array with "f" characters for food
+    worldLoader.readData(foodData, this.game.world);
+
+    let currentX = 10;
+    let currentY = 10;
+    for (let i = 0; i < totalPlayers; i++) {
+      const snake = new Snake(currentX, currentY);
+      const controller = new SnakeController(this.game.world, snake);
+
+      if (i < data.numOfHumanPlayers) {
+        const inputHandler = new LRKeyInputHandler();
+        players.push(new HumanPlayer(controller, inputHandler));
+      } else {
+        players.push(new AvoidWallsPlayer(controller));
+      }
+      this.game.players = players;
+      const canvasView = new CanvasWorldView(this.game.scalingFactor);
+      this.game.world.addView(canvasView);
+
+      this.run();
+    }
   }
 }
 export default GameController;
